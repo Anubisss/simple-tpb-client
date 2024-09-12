e0600c5952cad28872995abaf9776ea47d42b282
anuka <123344+Anubisss@users.noreply.github.com>
2024-09-12 18:02:36 +0200

    Bypass cache by adding a timestamp to the URLs

--- src/app/api/details/[id]/route.ts
+++ src/app/api/details/[id]/route.ts
@@ -4,9 +4,13 @@ import axios from 'axios';
 export const GET = async (req: Request, { params }: { params: { id: string } }) => {
   const { id } = params;
 
+  // bypass TPB's cache
+  const cacheTimestamp = new Date().getTime();
+  console.log(`https://apibay.org/t.php?id=${id}&timestamp=${cacheTimestamp}`);
+
   const [files, details] = await Promise.all([
-    axios.get(`https://apibay.org/f.php?id=${id}`),
-    axios.get(`https://apibay.org/t.php?id=${id}`),
+    axios.get(`https://apibay.org/f.php?id=${id}&timestamp=${cacheTimestamp}`),
+    axios.get(`https://apibay.org/t.php?id=${id}&timestamp=${cacheTimestamp}`),
   ]);
 
   return NextResponse.json({ files: files.data, details: details.data });
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,6 +3,12 @@ import axios from 'axios';
 
 export const POST = async (req: Request) => {
   const { q, cat } = await req.json();
-  const res = await axios.get(`https://apibay.org/q.php?q=${q}&cat=${cat}`);
+
+  // bypass TPB's cache
+  const cacheTimestamp = new Date().getTime();
+
+  const res = await axios.get(
+    `https://apibay.org/q.php?q=${q}&cat=${cat}&timestamp=${cacheTimestamp}`
+  );
   return NextResponse.json(res.data);
 };
